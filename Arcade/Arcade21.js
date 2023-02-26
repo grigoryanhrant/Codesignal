9aa488ddabcae9aadd672489b2048b5f79c884a2
grigoryanhrant <hgrigoryan755@gmail.com>
2023-02-26 07:09:41 -0800

    fix

--- Arcade/Arcade21.js
+++ Arcade/Arcade21.js
@@ -1,29 +1,22 @@
 const EMPTY_NUMBER = "";
-const NOTVALID_ZERO_1 = '00';
-const NOTVALID_ZERO_2 = '01';
+const NOT_VALID_ZERO_1 = '00';
+const NOT_VALID_ZERO_2 = '01';
 
-const minIP4vRange = 0;
-const maxIP4vRange = 255;
+const MIN_IPV4V_RANGE = 0;
+const MAX_IPV4V_RANGE = 255;
 
-const isValidIP4vRange = number => number >= minIP4vRange && number <= maxIP4vRange;
+const isValidIP4vRange = number => number >= MIN_IPV4V_RANGE && number <= MAX_IPV4V_RANGE;
 
-const checkEmpty = number => {
-    if(number === EMPTY_NUMBER) return false
+const numberIsEmpty = number => number !== EMPTY_NUMBER
 
-    return true
-}
+const notValidZerosCheck = number => number !== NOT_VALID_ZERO_1 && number !== NOT_VALID_ZERO_2
 
-const validIP4Exception = number => {
-    if(number === NOTVALID_ZERO_1 || number === NOTVALID_ZERO_2) return false
-    return true
-}
-
-const solution = string => {
-    let splittedString = string.split('.');
+const solution = ipAddress => {
+    let splittedString = ipAddress.split('.');
 
     if(splittedString.length !== 4) return false
 
-    for(let number of splittedString) if(!checkEmpty(number) || typeof Number(number) !== 'number' || !isValidIP4vRange(number) || !validIP4Exception(number)) return false
+    for(let number of splittedString) if(!numberIsEmpty(number) || typeof Number(number) !== 'number' || !isValidIP4vRange(number) || !notValidZerosCheck(number)) return false
 
     return true
 }
